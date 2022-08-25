696996acfea57ee14815f006ef078f53eeea0bcc
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2022-08-25 11:22:04 -0300

    Rotas e Parâmetros

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/users', (req, res) => {
+    const params = req.query
+    res.json(params)
+});
+
+app.post('/users/:id', (req, res) => {
+    const params = req.params
+    res.json(params)
+});
+
+app.post('/users', (req, res) => {
+    const params = req.body
+    res.json(params)
 });
 
 app.listen(3001); 
